2f33ff1cb4dc582d0e418dcc9a69c3477c3be231
root <root@ip-172-31-46-69.ap-south-1.compute.internal>
2022-03-29 11:01:12 +0000

    php code

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+ECHO "Hello!<br>";
+echo "Welcome to Develeping PHP <br>";
+EcHo "Enjoy all of the ad-free articles<br>";
+
+$name = "Saibaba";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+
+?>
+
